bf711f0f93ea51c41fab40d3d93f1e08d4a1df5f
tpn2024 <165146643+tpn2024@users.noreply.github.com>
2024-03-27 10:35:52 +0100

    Add files via upload
    Initial Commit

---
+++ src/main/java/com/company/sample/SampleInput.java
@@ -0,0 +1,12 @@
+package com.company.sample;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SampleInput {
+
+
+  static public List<String> alphabetlist = Arrays.asList("abc", "bba",null,  "ooo" ,"def", "kls", "asd", "abc1",null ,"bba1", "ooo1" ,"def1", "kls1", "asd1");
+
+  static public List<Integer> numberlist = Arrays.asList(1, 2, 3 ,4,5,6,7,8,9);
+}
---
+++ src/main/java/com/company/sample/misc/ForEachConsumer.java
@@ -0,0 +1,13 @@
+package com.company.sample.misc;
+
+
+
+public class ForEachConsumer {
+
+    public static void main(String[] args) {
+
+// changes to be added here
+
+    }
+
+}
